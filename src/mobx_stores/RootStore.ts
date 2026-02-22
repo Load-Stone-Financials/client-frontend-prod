af7257cd24044457a396c55dd012eb81fe4c2e35
Mheyrie <omota4fidelity@gmail.com>
2026-02-22 20:11:39 +0100

    updated

--- src/mobx_stores/RootStore.ts
+++ src/mobx_stores/RootStore.ts
@@ -1,26 +1,23 @@
-
 import { AuthStore } from "@/mobx_stores/AuthStore";
 import { ProfileStore } from "@/mobx_stores/ProfileStore";
 import { createContext } from "react";
 
-
-
 interface StoreContextInterface {
   authStore: AuthStore;
-//   guarantorStore: GuarantorStore;
   profileStore: ProfileStore;
-//   walletStore: WalletStore;
-//   investStore: InvestStore;
-//   billStore: BillStore;
-//   blogStore: BlogStore;
-//   invoiceStore: InvoiceStore;
-//   nWalletStore: NWalletStore;
+  //   guarantorStore: GuarantorStore;
+  //   walletStore: WalletStore;
+  //   investStore: InvestStore;
+  //   billStore: BillStore;
+  //   blogStore: BlogStore;
+  //   invoiceStore: InvoiceStore;
+  //   nWalletStore: NWalletStore;
 }
 
 export const authStore = new AuthStore();
+const profileStore = new ProfileStore();
 // const loansStore = new LoansStore();
 // const guarantorStore = new GuarantorStore();
-const profileStore = new ProfileStore();
 // const walletStore = new WalletStore();
 // const investStore = new InvestStore();
 // const billStore = new BillStore();
@@ -29,27 +26,27 @@ const profileStore = new ProfileStore();
 // const nWalletStore = new NWalletStore();
 
 export const StoreContext = createContext<StoreContextInterface>({
-    authStore,
-    profileStore,
-//   loansStore,
-//   guarantorStore,
-//   profileStore,
-//   walletStore,
-//   investStore,
-//   billStore,
-//   blogStore,
-//   invoiceStore,
-//   nWalletStore,
+  authStore,
+  profileStore,
+  //   loansStore,
+  //   guarantorStore,
+  //   profileStore,
+  //   walletStore,
+  //   investStore,
+  //   billStore,
+  //   blogStore,
+  //   invoiceStore,
+  //   nWalletStore,
 });
 
 export const SetAllAccessTokens = (token: string) => {
-      authStore.SetAccessToken(token);
-//   loansStore.SetAccessToken(token);
-//   guarantorStore.SetAccessToken(token);
-//   profileStore.SetAccessToken(token);
-//   walletStore.SetAccessToken(token);
-//   investStore.SetAccessToken(token);
-//   billStore.SetAccessToken(token);
-//   invoiceStore.SetAccessToken(token);
-//   nWalletStore.SetAccessToken(token);
+  authStore.SetAccessToken(token);
+  //   loansStore.SetAccessToken(token);
+  //   guarantorStore.SetAccessToken(token);
+  //   profileStore.SetAccessToken(token);
+  //   walletStore.SetAccessToken(token);
+  //   investStore.SetAccessToken(token);
+  //   billStore.SetAccessToken(token);
+  //   invoiceStore.SetAccessToken(token);
+  //   nWalletStore.SetAccessToken(token);
 };
