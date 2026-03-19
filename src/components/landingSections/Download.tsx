51eb8665b16c5a2345eb315dbc1b58254b699245
Mheyrie <omota4fidelity@gmail.com>
2026-03-19 22:49:24 +0100

    feat: Introduce mobile app download and newsletter sections, and enhance Business landing page with loan application process details.

---
+++ src/components/landingSections/Download.tsx
@@ -0,0 +1,67 @@
+import BaseDirectories from "@/baseDir/baseDirectories";
+import Button from "../ui/Button";
+
+export default function Download() {
+  return (
+    <div>
+      <div className="bg-brand-purple-light flex w-full text-white text-center">
+        <div className="max-w-xl mx-auto flex justify-end">
+          <img
+            src={`${BaseDirectories.IMAGES_DIR}/landing/business/iphone.png`}
+            alt=""
+          />
+        </div>
+        <div className="max-w-3xl mx-auto flex flex-col justify-center items-start">
+          <h2 className="text-6xl font-bold mb-2 max-w-xl text-start">Download Our Mobile App!</h2>
+          <p className="text-start text-lg  max-w-[320px]">
+            Create an account to access our tailored financing solutions in no
+            time.
+          </p>
+          <div className="flex justify-center items-center gap-2 flex-wrap">
+            <img
+              src={`${BaseDirectories.IMAGES_DIR}/landing/business/ios.png`}
+              alt=""
+            />
+            <img
+              src={`${BaseDirectories.IMAGES_DIR}/landing/business/andriod.png`}
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      {/* Newsletter Section */}
+      <div className="bg-brand-purple-light  w-full lg:h-[248px] py-10 px-4 text-white text-center">
+        <div className="lg:h-[200px} max-w-[1250px] mx-auto pt-37px">
+          <h2 className="text-2xl  font-semibold fw-600 text-[40px] mb-2">
+            Subscribe to our Newsletter
+          </h2>
+          <p className="mb-4 text-[24px] fw-500 ">
+            Subscribe to our newsletter to receive updates, exclusive offers,
+            news, and more.
+          </p>
+          <div className="flex justify-center items-center gap-2 flex-wrap">
+            <div className="relative">
+              <span className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400">
+                <img
+                  src={`${BaseDirectories.ICONS_DIR}/Mail.png`}
+                  alt="Mail icon"
+                />
+              </span>
+
+              <input
+                type="email"
+                placeholder="Email address"
+                className="bg-white pl-4 pr-3 py-2  lg:w-[393px] rounded border border-gray-300 text-black focus:outline-none"
+              />
+            </div>
+            <Button
+              classes="primary-btn btn-sm !py-5 "
+              content="Subscribe"
+              onClick={() => {}}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/landing/Business.tsx
+++ src/pages/landing/Business.tsx
@@ -1,9 +1,77 @@
+import LoanCalc from "@/components/landingSections/LoanCalc";
 import LandingLayout from "../../components/_shared/LandingLayout";
+import Button from "@/components/ui/Button";
+import toast from "react-hot-toast";
+
+import Download from "@/components/landingSections/Download";
 
 export default function Business() {
   return (
     <LandingLayout>
-   <section className="h-screen text-3xl flex justify-center items-center">This is Business page</section>
+      <section className="h-screen text-3xl flex justify-center items-center">
+        This is Business page
+      </section>
+      <div className="max-w-xl mx-auto">
+        <LoanCalc />
+      </div>
+      <div
+        className="flex flex-col justify-center items-center px-6 py-10 gap-2"
+        data-aos="fade-right"
+        data-aos-offset="70"
+        data-aos-delay="0"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out"
+        data-aos-mirror="true"
+        data-aos-once="true"
+      >
+       <div className="flex justify-between items-center gap-6">
+          <div className="flex justify-start items-center gap-2 mb-2">
+            <div className="before:content-[''] before:block before:w-2 before:h-32 before:bg-brand-purple before:mx-auto before:my-4" />
+            <div className="text-start">
+              <h2 className="text-brand-purple text-4xl font-bold mb-2">
+                Create an account
+              </h2>
+              <p className="text-md flex items-center justify-start max-w-[320px]">
+                Create an Account with LoadStone and access funding to grow your
+                business
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-start items-center gap-2 mb-2">
+            <div className="before:content-[''] before:block before:w-2 before:h-32 before:bg-[#EAA942] before:mx-auto before:my-4" />
+            <div className="text-start">
+              <h2 className="text-[#EAA942] text-4xl font-bold mb-2">
+                Complete profile
+              </h2>
+              <p className="text-md flex items-center justify-start max-w-[320px]">
+                Complete your profile by telling about your business and uploading
+                documents to verify your business.
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-start items-center gap-2 mb-2">
+            <div className="before:content-[''] before:block before:w-2 before:h-24 before:bg-brand-purple before:mx-auto before:my-4" />
+            <div className="text-start">
+              <h2 className="text-brand-purple text-4xl font-bold mb-2">
+                Apply for loan
+              </h2>
+              <p className="text-md flex items-center justify-start max-w-[320px]">
+                Apply for our lending financial solution no long forms or
+                collateral
+              </p>
+            </div>
+          </div>
+       </div>
+        <div className=" flex items-center justify-center w-full mt-8">
+          <Button
+            content="Take Loan"
+            classes="primary-btn btn-md w-full "
+            onClick={() => toast.success("Coming soon")}
+          />
+        </div>
+      </div>
+      
+      <Download />
     </LandingLayout>
   );
 }
